aaba7fa54eba3009f61e43b7c79752dc00301e14
Austin Johanningsmeier <ajohanni@umich.edu>
2019-11-23 16:01:04 -0500

    Added unit tests for Capitalize and DB

---
+++ Example.WebAPI/StatTests/DbTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Example.DataAccess;
+using NUnit.Framework;
+
+namespace StatTests
+{
+    [TestFixture]
+    class DbTests
+    {
+        DBInteraction db;
+
+        [SetUp]
+        public void Setup()
+        {
+            db = new DBInteraction();
+        }
+
+        [Test]
+        public void GetWordUse()
+        {
+            //Arrange
+            int result;
+            string word = "string";
+
+            //Act
+            result = db.GetWordUse(word);
+
+            //Assert
+            Assert.That(result > 0);
+        }
+
+        [Test]
+        public void GetCharUse()
+        {
+            //Arrange
+            int result;
+            char letter = 'e';
+
+            //Act
+            result = db.GetCharUse(letter);
+
+            //Assert
+            Assert.That(result > 0);
+        }
+    }
+}
