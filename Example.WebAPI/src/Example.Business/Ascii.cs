8be20a5c0de714132c087f06b1fb9ee82bf93cf0
Ed <embate@umich.edu>
2019-12-15 21:43:06 -0500

    Merge branch 'Binary'

---
+++ Example.WebAPI/src/Example.Business/Ascii.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using Example.Business.Interfaces;
+using Example.Business.Models;
+using System.Collections.Generic;
+
+namespace Example.Business
+{
+    public class Ascii : IAscii
+    {
+        public AsciiResponse ProccessRequest(AsciiRequest request)
+        {
+            AsciiResponse response = new AsciiResponse();
+            response.originalString = request.stringToModify;
+            string stringInProgress = request.stringToModify;
+            String responseString = "";
+
+    
+            byte[] arr = Encoding.ASCII.GetBytes(stringInProgress);
+
+            foreach (byte bit in arr)
+            {
+                responseString += (" {0} ", bit, (char)bit);
+            }
+            response.modifiedString = responseString;
+
+            return response;
+        }
+
+    }
+}
---
+++ Example.WebAPI/src/Example.Business/Models/AsciiResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Example.Business.Models
+{
+    public class AsciiResponse
+    {
+        public string modifiedString { get; set; }
+        public string originalString { get; set; }
+    }
+}
